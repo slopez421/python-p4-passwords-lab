dce6e7fed88d4bbd24c6c8cc2f9614f338b704ba
Shania Lopez <slopez@fandm.edu>
2024-07-08 15:30:05 -0400

    lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,34 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+       
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        password = request.get_json()['password']
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+           
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
